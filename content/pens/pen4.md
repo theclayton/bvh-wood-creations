dc472a3e1e490e2a1a9fa07bf6753e74690bb110
Clayton Ward <clayton@flaresoftware.com>
2022-05-17 18:49:05 -0700

    Update product page

--- content/pens/pen4.md
+++ content/pens/pen4.md
@@ -1,11 +1,11 @@
 ---
 title: Pen 4
 subtitle: Real nice pen
-etsyLink: https://etsy.com
+etsyLink: https://www.etsy.com/listing/966438750/bolt-action-hunters-collection?click_key=6ecce1b871f115c494d408d7b99a522c0cf48632%3A966438750&click_sum=f3b82b80&ref=shop_home_feat_1&frs=1
 thumbnail: /images/uploads/pen.jpeg
-images: ["image1", "image2"]
+images: ["/images/uploads/pen.jpeg", "/images/uploads/pen.jpeg"]
 ---
 
 ### Description
 
-Detailed decription and stuff
+Detailed description of this product coming soon!
--- pages/pens/_slug.vue
+++ pages/pens/_slug.vue
@@ -1,20 +1,34 @@
 <template>
   <v-container class="mt-16">
-    <div class="mt-10">
+    <div class="mt-10 mb-2">
       <NuxtLink to="/shop">Shop</NuxtLink>
       / {{ pen.slug }}
     </div>
 
-    <h1 class="mt-7">{{ pen.title }}</h1>
-    <h2 class="mt-2">{{ pen.subtitle }}</h2>
+    <v-row no-gutters class="mb-5">
+      <v-col cols="12" sm="6" md="8">
+        <v-carousel>
+          <v-carousel-item
+            v-for="(img, i) in pen.images"
+            :key="i"
+            :src="img"
+          ></v-carousel-item>
+        </v-carousel>
+      </v-col>
 
-    <a class="text-decoration-none" :href="pen.etsyLink" target="_blank">
-      <v-btn class="my-5">View on Etsy </v-btn>
-    </a>
+      <v-col cols="6" md="4" class="pl-3">
+        <h1 class="text-h3 font-weight-bold">{{ pen.title }}</h1>
+        <h2 class="pb-6 text-h5 font-weight-light">{{ pen.subtitle }}</h2>
 
-    <p v-for="(img, i) in pen.images" :key="i">{{ img }}</p>
+        <v-divider class="pb-3"></v-divider>
 
-    <nuxt-content :document="pen" />
+        <nuxt-content :document="pen" />
+
+        <a class="text-decoration-none" :href="pen.etsyLink" target="_blank">
+          <v-btn class="my-5">View on Etsy </v-btn>
+        </a>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
