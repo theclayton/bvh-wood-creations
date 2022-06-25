3dc451d5e91d40d819b644da71a5f85924e10e01
Clayton Ward <clayton@flaresoftware.com>
2022-06-25 12:52:28 -0700

    remove hidden field

--- pages/index.vue
+++ pages/index.vue
@@ -107,76 +107,12 @@
       <h2>{{ contactHeading }}</h2>
       <h4 class="pb-10">{{ contactPhone }}</h4>
 
-      <!-- CONTACT FORM -->
-      <div>
-        <v-row justify="center">
-          <v-col md="5">
-            <p class="text-subtitle-1">Contact us with the form below:</p>
-
-            <v-form
-              name="bvh-contact-form"
-              method="POST"
-              ref="form"
-              lazy-validation
-              data-netlify="true"
-              data-netlify-honeypot="bot-field"
-              v-on:submit.prevent="onSubmit"
-            >
-              <input type="hidden" name="bvh-form" value="bvh-contact-form" />
-
-              <v-text-field
-                placeholder="First Name"
-                solo
-                clearable
-                v-model="form.firstName"
-                :rules="nameRules"
-              ></v-text-field>
-
-              <v-text-field
-                placeholder="Last Name"
-                solo
-                clearable
-                v-model="form.lastName"
-                :rules="nameRules"
-              ></v-text-field>
-
-              <v-text-field
-                placeholder="Email Address"
-                solo
-                clearable
-                v-model="form.email"
-                :rules="emailRules"
-              ></v-text-field>
-
-              <v-textarea
-                placeholder="Message..."
-                solo
-                clearable
-                v-model="form.message"
-              ></v-textarea>
-
-              <p :v-show="error" class="red--text">{{ error }}</p>
-              <v-btn
-                :disabled="isLoading"
-                type="submit"
-                large
-                rounded
-                color="primaryBlue"
-                class="text-decoration-none mt-1 mb-16 white--text"
-                on
-              >
-                {{ submitButtonText }}
-              </v-btn>
-            </v-form>
-          </v-col>
-        </v-row>
-      </div>
+      <contact-form></contact-form>
     </v-container>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 import content from "../content/pages/index.json";
 
 export default {
@@ -193,30 +129,7 @@ export default {
     };
   },
   data() {
-    return {
-      ...content,
-      isLoading: false,
-      submitButtonText: "Send Message",
-      error: "",
-      form: {
-        "form-name": "bvh-contact-form",
-        firstName: "",
-        lastName: "",
-        email: "",
-        message: "",
-      },
-      nameRules: [
-        (v) => !!v || "Name is required",
-        (v) => (v && v.length <= 20) || "Name must be less than 10 characters",
-      ],
-      emailRules: [
-        (v) => !!v || "Email is required",
-        (v) =>
-          /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()\\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-            v
-          ) || "Email must be valid",
-      ],
-    };
+    return content;
   },
   async asyncData({ $content, params, error }) {
     let featured1;
@@ -232,41 +145,6 @@ export default {
       featured2,
       featured3,
     };
-  },
-  methods: {
-    encode(data) {
-      return Object.keys(data)
-        .map(
-          (key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
-        )
-        .join("&");
-    },
-    async onSubmit() {
-      if (this.$refs.form.validate()) {
-        this.isLoading = true;
-        this.submitButtonText = "Sending...";
-
-        try {
-          await axios({
-            method: "post",
-            url: "/",
-            data: this.encode(this.form),
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          });
-
-          // await axios.post("/", this.encode(this.form), {
-          //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          // });
-
-          this.error = "";
-          this.submitButtonText = "Sent!";
-        } catch (e) {
-          this.error = e.message;
-          this.isLoading = false;
-          this.submitButtonText = "Failed, try again?";
-        }
-      }
-    },
   },
 };
 </script>
